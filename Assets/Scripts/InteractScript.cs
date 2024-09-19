73b3d40e6ed99bb58c2af6b0afd43fea6f8b9b0d
kenkaster001 <jm.amador.t@gmail.com>
2024-09-19 22:16:51 +0800

    distance checker implementation
    checks if the player is in the "interactable" distance

--- Assets/Scripts/InteractScript.cs
+++ Assets/Scripts/InteractScript.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class InteractScript : MonoBehaviour
 {
+    [SerializeField] GameObject prompt;
     public bool notDiscovered = true;
     public GameObject symbol;
     public GameObject description;
 
     public GameObject player;
 
-    [SerializeField] private float _interactionPointRadius = 0.5f;
+    [SerializeField] private float _interactionPointRadius = 1.7f;
     [SerializeField] private Transform  _interactionPoint;
     [SerializeField] private LayerMask _interactableMask;
+
+    [SerializeField] private GameObject notebook;
 
     private readonly Collider[] _colliders = new Collider[3];
     [SerializeField] private int _numfound;
@@ -20,18 +23,23 @@ public class InteractScript : MonoBehaviour
     void Start()
     {
         description.SetActive(false);
+        prompt.SetActive(false);
     }
 
     void Update()
     {
+        float distance = (_interactionPoint.position - player.transform.position).magnitude;
         _numfound = Physics.OverlapSphereNonAlloc(_interactionPoint.position, _interactionPointRadius, _colliders, _interactableMask);
-        if (notDiscovered == true)
+        
+        if (notDiscovered == true && (distance <= _interactionPointRadius))
         {
+            prompt.SetActive(true);
             if (Input.GetKeyDown(KeyCode.E))
             {
                 symbol.SetActive(false);
                 description.SetActive(true);
                 notDiscovered = false;
+                notebook.SetActive(true);
             }
             
         } else
@@ -48,4 +56,5 @@ public class InteractScript : MonoBehaviour
     {
         Gizmos.DrawWireSphere(_interactionPoint.position, _interactionPointRadius);
     }
+
 }
--- Assets/Scripts/SC_FPSController.cs
+++ Assets/Scripts/SC_FPSController.cs
@@ -96,7 +96,7 @@ public class SC_FPSController : MonoBehaviour
                 canMove = true;
             }
         }
-
+        
         
     }
 
