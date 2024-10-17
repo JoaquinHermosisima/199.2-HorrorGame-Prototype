b0574800e837e74ed4d36864a8c63212b7b3fc89
Neal Rodriguez <neal.rodriguez@obf.ateneo.edu>
2024-10-18 00:09:13 +0800

    Door BugFixed-ish

--- Assets/Scripts/Char_Interactible.cs
+++ Assets/Scripts/Char_Interactible.cs
@@ -9,66 +9,111 @@ using UnityEditor.Animations;
 
 public class Char_Interactible : MonoBehaviour
 {
-    public GameObject charCanvas;
-    public GameObject charBlock;
-    public TMP_InputField inputField;
-    public TMP_Text textDisplay;
-    public Button submitButton;
+    public GameObject charCanvas_1;
+    public GameObject charCanvas_2;
+    public GameObject door;
+    public TMP_InputField inputField_1;
+    public TMP_InputField inputField_2;
+    public TMP_Text textDisplay_1;
+    public TMP_Text textDisplay_2;
+    public Button submitButton_1;
+    public Button submitButton_2;
     //public string input;
-    public string correctChar;
-    public string answer;
+    public string correctChar_1;
+    public string correctChar_2;
+    public string answer_1;
+    public string answer_2;
     public bool isAnswered;
-    public bool triggered;
+    
     private void Start()
     {
-        charCanvas.SetActive(false);
+        charCanvas_1.SetActive(false);
+        charCanvas_2.SetActive(false);
         isAnswered = false;
-        triggered = false;
     }
 
     private void Update()
     {
         if (isAnswered)
         {
-            charBlock.SetActive(false);
+            door.SetActive(false);
         }
     }
     public void Interact()
     {
-        charCanvas.SetActive(true);
-        Cursor.lockState = CursorLockMode.None;
-        submitButton.onClick.AddListener(evalInput);
+        System.Random rnd = new System.Random();
+        int num = rnd.Next(0, 2);
+        Debug.Log(num);
+        if (num == 0)
+        {
+            charCanvas_1.SetActive(true);
+            Cursor.lockState = CursorLockMode.None;
+            submitButton_1.onClick.AddListener(evalInput0);
+        }
+        else if (num == 1) {
+            charCanvas_2.SetActive(true);
+            Cursor.lockState = CursorLockMode.None;
+            submitButton_2.onClick.AddListener(evalInput1);
+        }
+        
     }
 
     public void dontInteract()
     {
-        charCanvas.SetActive(false);
+        charCanvas_1.SetActive(false);
+        charCanvas_2.SetActive(false);
         Cursor.lockState = CursorLockMode.Locked;
     }
 
-    public void evalInput()
+    public void evalInput0()
     {
-        answer = inputField.text;
-        answer = answer.ToUpper();
-        if (answer == correctChar)
+        answer_1 = inputField_1.text;
+        answer_1 = answer_1.ToUpper();
+        if (answer_1 == correctChar_1)
         {
-            textDisplay.SetText("Correct");
+            textDisplay_1.SetText("Correct");
         }
         else
         {
-            textDisplay.SetText("WRONG");
+            textDisplay_1.SetText("WRONG");
         }
-        StartCoroutine(DestroyCanvas());
-
+        StartCoroutine(DestroyCanvas0());
     }
 
-    IEnumerator DestroyCanvas()
+    public void evalInput1()
+    {
+        answer_2 = inputField_2.text;
+        answer_2 = answer_2.ToUpper();
+        if (answer_2 == correctChar_2)
+        {
+            textDisplay_2.SetText("Correct");
+        }
+        else
+        {
+            textDisplay_2.SetText("WRONG");
+        }
+        StartCoroutine(DestroyCanvas1());
+    }
+
+    IEnumerator DestroyCanvas0()
     {
         yield return new WaitForSeconds(1);
-        charCanvas.SetActive(false);
-        textDisplay.SetText("what character is this");
-        inputField.text = "";
-        if(answer == correctChar)
+        charCanvas_1.SetActive(false);
+        textDisplay_1.SetText("what character is this");
+        inputField_1.text = "";
+        if(answer_1 == correctChar_1)
+        {
+            isAnswered = true;
+        }
+    }
+
+    IEnumerator DestroyCanvas1()
+    {
+        yield return new WaitForSeconds(1);
+        charCanvas_2.SetActive(false);
+        textDisplay_2.SetText("what character is this");
+        inputField_2.text = "";
+        if (answer_2 == correctChar_2)
         {
             isAnswered = true;
         }
