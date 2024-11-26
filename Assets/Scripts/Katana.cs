a4ac55a7b36ea62d74298329d8b50739721bfb4e
Neal Rodriguez <neal.rodriguez@obf.ateneo.edu>
2024-11-26 23:41:00 +0800

    Katana Puzzle Tables and Paintings added

--- Assets/Scripts/Katana.cs
+++ Assets/Scripts/Katana.cs
@@ -6,8 +6,22 @@ public class Katana : MonoBehaviour
 {
     //This class is for the Katana
     private int currentAngle = 0;
-    private readonly int[] angles = { 45, 90, 135, 180, 225, 270, 315, 360 };
+    private readonly int[] angles = { 0, 70, 130, 155, 180, 240, 310, 335};
+    public Material currentMaterial;
+    public Material greenGlow;
+    [SerializeField] private KatanaTable table;
+    [SerializeField] private int correctAngle;
 
+    private void Update()
+    {
+        if(angles[currentAngle] == correctAngle)
+        {
+            table.GetComponent<Renderer>().material = greenGlow;
+        } else
+        {
+            table.GetComponent<Renderer>().material = currentMaterial;
+        }
+    }
     public void Rotate()
     {
         currentAngle = (currentAngle + 1) % angles.Length;
