477f6327764ee971e5ded5cf2e335b43e8ef37f3
Neal Rodriguez <neal.rodriguez@obf.ateneo.edu>
2024-11-27 00:14:34 +0800

    Note and NonIntCanvases added

--- Assets/Scripts/Katana.cs
+++ Assets/Scripts/Katana.cs
@@ -6,7 +6,7 @@ public class Katana : MonoBehaviour
 {
     //This class is for the Katana
     private int currentAngle = 0;
-    private readonly int[] angles = { 0, 70, 130, 155, 180, 240, 310, 335};
+    private readonly int[] angles = {0, 70, 130, 155, 180, 240, 310, 335};
     public Material currentMaterial;
     public Material greenGlow;
     [SerializeField] private KatanaTable table;
