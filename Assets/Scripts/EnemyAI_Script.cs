a61be127d17de067d9b40db969a13596ea010801
Neal Rodriguez <neal.rodriguez@obf.ateneo.edu>
2024-11-30 00:01:06 +0800

    AI and Lantern Fixed

--- Assets/Scripts/EnemyAI_Script.cs
+++ Assets/Scripts/EnemyAI_Script.cs
@@ -8,7 +8,7 @@ public class EnemyAI : MonoBehaviour
 {
     public NavMeshAgent ai;
     public List<Transform> destinations;
-    public Animator aiAnim;
+    //public Animator aiAnim;
     public float walkSpeed, chaseSpeed, minIdleTime, maxIdleTime, idleTime, sightDistance, catchDistance, chaseTime, minChaseTime, maxChaseTime, jumpscareTime;
     public bool walking, chasing;
     public Transform player;
@@ -45,17 +45,17 @@ public class EnemyAI : MonoBehaviour
             dest = player.position;
             ai.destination = dest;
             ai.speed = chaseSpeed;
-            aiAnim.ResetTrigger("walk");
+            /*aiAnim.ResetTrigger("walk");
             aiAnim.ResetTrigger("idle");
-            aiAnim.SetTrigger("sprint");
+            aiAnim.SetTrigger("sprint");*/
             float distance = Vector3.Distance(player.position, ai.transform.position);
             if (distance <= catchDistance)
             {
                 player.gameObject.SetActive(false);
-                aiAnim.ResetTrigger("walk");
+                /*aiAnim.ResetTrigger("walk");
                 aiAnim.ResetTrigger("idle");
                 aiAnim.ResetTrigger("sprint");
-                aiAnim.SetTrigger("jumpscare");
+                aiAnim.SetTrigger("jumpscare");*/
                 StartCoroutine(deathRoutine());
                 chasing = false;
             }
@@ -65,14 +65,14 @@ public class EnemyAI : MonoBehaviour
             dest = currentDest.position;
             ai.destination = dest;
             ai.speed = walkSpeed;
-            aiAnim.ResetTrigger("sprint");
+            /*aiAnim.ResetTrigger("sprint");
             aiAnim.ResetTrigger("idle");
-            aiAnim.SetTrigger("walk");
+            aiAnim.SetTrigger("walk");*/
             if (ai.remainingDistance <= ai.stoppingDistance)
             {
-                aiAnim.ResetTrigger("sprint");
+                /*aiAnim.ResetTrigger("sprint");
                 aiAnim.ResetTrigger("walk");
-                aiAnim.SetTrigger("idle");
+                aiAnim.SetTrigger("idle");*/
                 ai.speed = 0;
                 StopCoroutine("stayIdle");
                 StartCoroutine("stayIdle");
