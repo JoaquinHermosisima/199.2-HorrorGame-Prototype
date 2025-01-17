551ddda2f7ec2282937d5996d3c68784feb0f6d4
Neal Rodriguez <neal.rodriguez@obf.ateneo.edu>
2025-01-18 01:29:20 +0800

    Brute force removed from Katanas

--- Assets/Scripts/Katana.cs
+++ Assets/Scripts/Katana.cs
@@ -7,10 +7,7 @@ public class Katana : MonoBehaviour
     //This class is for the Katana
     public int currentAngle = 0;
     private readonly int[] angles = {0, 70, 130, 155, 180, 240, 310, 335};
-    public Material currentMaterial;
-    public Material greenGlow;
     public bool isCorrect = false;
-    [SerializeField] private KatanaTable table;
     [SerializeField] public int correctAngle;
     private void Start()
     {
@@ -21,11 +18,9 @@ public class Katana : MonoBehaviour
         if(angles[currentAngle] == correctAngle)
         {
             isCorrect = true;
-            table.GetComponent<Renderer>().material = greenGlow;
         } else
         {
             isCorrect = false;
-            table.GetComponent<Renderer>().material = currentMaterial;
         }
     }
     public void Rotate()
