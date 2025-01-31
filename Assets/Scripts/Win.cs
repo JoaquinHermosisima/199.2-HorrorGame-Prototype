f3eef680f8e6179fdcd24e067350bab75c8afc2d
Neal Rodriguez <neal.rodriguez@obf.ateneo.edu>
2025-02-01 01:57:07 +0800

    You Win Screen Works

--- Assets/Scripts/Win.cs
+++ Assets/Scripts/Win.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Win : MonoBehaviour
 {
@@ -27,11 +28,7 @@ public class Win : MonoBehaviour
 
         if (occupied)
         {
-            canvas.SetActive(true);
-        }
-        else
-        {
-            canvas.SetActive(false);
+            SceneManager.LoadScene("Win_Scene");
         }
 
     }
